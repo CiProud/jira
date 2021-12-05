6400933b6a72440d151e459266917343712c0d49
CiProud <sunshihao12@163.com>
2021-12-05 14:52:41 +0800

    chore: 改变setSearchParams实现

--- src/util/url.ts
+++ src/util/url.ts
@@ -5,7 +5,8 @@ import { cleanObject } from "util/index";
 /*返回url中参数值
  */
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
   const [stateKeys] = useState(keys);
   return [
     useMemo(
@@ -16,11 +17,7 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
       [searchParams, stateKeys]
     ),
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({
-        ...Object.fromEntries(searchParams),
-        ...params,
-      }) as URLSearchParamsInit;
-      return setSearchParam(o);
+      return setSearchParams(params);
     },
   ] as const;
 };
